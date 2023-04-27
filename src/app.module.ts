17b151cbb8b5e389f4dd370286146d9a9ce58891
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-04-27 17:07:09 +0800

    remove old imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
