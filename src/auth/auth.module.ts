202467ae548f9d3c3d279af42c2cdb51899314ac
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-03 13:00:11 +0800

    auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { VatsimStrategy } from './vatsim/vatsim.strategy';
+import { JwtStrategy } from './jwt/jwt.strategy';
+
+@Module({
+    imports: [
+        UsersModule,
+        HttpModule,
+        PassportModule,
+        JwtModule.registerAsync({
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    secret: configService.get<string>('JWT_SECRET'),
+                    signOptions: { expiresIn: '1h' },
+                };
+            },
+            inject: [ConfigService],
+        }),
+    ],
+    providers: [AuthService, VatsimStrategy, JwtStrategy],
+    controllers: [AuthController],
+})
+export class AuthModule {}
