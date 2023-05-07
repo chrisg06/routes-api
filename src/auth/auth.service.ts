6603b6f3684e330fe470871743d9fc2d9c4a6103
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-07 14:14:15 +0800

    format auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,67 +8,68 @@ import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private userService: UsersService,
-        private jwtService: JwtService,
-        private configService: ConfigService,
-        private http: HttpService,
-    ) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private http: HttpService,
+    private userService: UsersService,
+  ) {}
 
-    getTokens(user: any) {
-        const payload = { sub: user.id };
+  getTokens(user: any) {
+    const payload = { sub: user.user._id };
 
-        const refreshToken = user.refreshToken;
-        delete user.refreshToken;
+    const refreshToken = user.refreshToken;
+    delete user.refreshToken;
 
-        return {
-            token: this.jwtService.sign(payload),
-            expiresIn: 60 * 60 * 1000, // 1 hour
-            refreshToken,
-        };
+    return {
+      token: this.jwtService.sign(payload),
+      expiresIn: 60 * 60 * 1000,
+      refreshToken,
+    };
+  }
+  async refresh(refreshToken: string) {
+    const params = new URLSearchParams({
+      grant_type: 'refresh_token',
+      client_id: this.configService.get<string>('VATSIM_AUTH_CLIENT_ID'),
+      client_secret: this.configService.get<string>(
+        'VATSIM_AUTH_CLIENT_SECRET',
+      ),
+      refresh_token: refreshToken,
+    });
+
+    const tokenUrl = this.configService.get<string>('VATSIM_AUTH_TOKEN_URL');
+    const $creds = this.http.post(tokenUrl, params.toString(), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    let creds: any;
+    try {
+      creds = (await firstValueFrom($creds)).data;
+    } catch (err) {
+      throw new UnauthorizedException();
     }
 
-    async refresh(refreshToken: string) {
-        const params = new URLSearchParams({
-            grant_type: 'refresh_token',
-            client_id: this.configService.get<string>('VATSIM_AUTH_CLIENT_ID'),
-            client_secret: this.configService.get<string>('VATSIM_AUTH_CLIENT_SECRET'),
-            refresh_token: refreshToken,
-        });
+    const userUrl = this.configService.get<string>('VATSIM_AUTH_USER_INFO_URL');
+    const $user = this.http.get(userUrl, {
+      headers: {
+        Authorization: `Bearer ${creds.access_token}`,
+      },
+    });
 
-        const tokenUrl = this.configService.get<string>('VATSIM_AUTH_TOKEN_URL');
-        const $creds = this.http.post(tokenUrl, params.toString(), {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        });
+    try {
+      const ssoUser = (await firstValueFrom($user)).data.data;
+      const user = await this.userService.findByCid(ssoUser.cid);
 
-        let creds: any
-        try {
-            creds = (await firstValueFrom($creds)).data;
-        } catch (err) {
-            throw new UnauthorizedException();
-        }
-
-        const userUrl  = this.configService.get<string>('VATSIM_AUTH_USER_INFO_URL');
-        const $user = this.http.get(userUrl, {
-            headers: {
-                Authorization: `Bearer ${creds.access_token}`,
-            },
-        });
-
-        try {
-            const ssoUser = (await firstValueFrom($user)).data.data;
-            const user = await this.userService.findByCid(ssoUser.cid);
-            
-            return {
-                user,
-                token: this.jwtService.sign({ sub: user.id }),
-                expiresIn: 60 * 60 * 1000, // 1 hour
-                newRefreshToken: creds.refresh_token,
-            };
-        } catch (err) {
-            throw new UnauthorizedException();
-        }
+      return {
+        user,
+        token: this.jwtService.sign({ sub: user.id }),
+        expiresIn: 60 * 60 * 1000, // 1 hour
+        newRefreshToken: creds.refresh_token,
+      };
+    } catch (err) {
+      throw new UnauthorizedException();
     }
+  }
 }
