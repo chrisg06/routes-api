2dc8fc3a4e7aa8eaf518ed9258384c1b95a5cdfb
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-07 14:35:04 +0800

    setup main.ts for cookies

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import * as cookieparser from 'cookie-parser';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const cookieSecret = configService.get('COOKIE_SECRET');
+  app.use(cookieparser(cookieSecret))
 
   app.enableCors({
     origin: [/vatpac\.org$/, /localhost(:\d+)?$/],
     credentials: true,
   });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
   .setTitle('Routes API')
