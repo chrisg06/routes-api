d40ac990b050db701e4aa0c79acb366f1c8ec059
Chris Gardiner <chris.gardiner06@outlook.com>
2023-05-18 00:37:24 +0930

    Update routes.controller.ts

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -28,22 +28,40 @@ export class RoutesController {
     const { count, routes } = await this.routesService.findAll(dept, dest);
     return { count, routes: routes };
   }
-
+  /**
+   * Gets route by id
+   * @param id 
+   * @returns 
+   */
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<Route> {
     return await this.routesService.findOne(id);
   }
 
+  /**
+   * Creates a new route
+   * @param route 
+   * @returns 
+   */
   @Post()
   async createRoute(@Body() route: CreateRouteDto): Promise<Route> {
     return await this.routesService.createRoute(route);
   }
 
+  /** 
+   * Uploads route data
+   */
   @Post('data')
   async postData(@Body() data: UploadRoutesDto): Promise<any> {
     return await this.routesService.postData(data);
   }
 
+  /**
+   * Update a route
+   * @param id 
+   * @param routeData 
+   * @returns 
+   */
   @Patch(':id')
   async editRoute(
     @Param('id') id: number,
@@ -58,6 +76,11 @@ export class RoutesController {
     return await this.routesService.updateRoute(route, routeData);
   }
 
+  /**
+   * Delete a route
+   * @param id 
+   * @returns 
+   */
   @Delete(':id')
   async deleteRoute(@Param('id') id: number): Promise<Route> {
     const route = await this.routesService.findOne(id);
