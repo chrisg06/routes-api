b94f1e2b324729775d72aef6ace500dc0fadf054
Chris Gardiner <chris.gardiner06@outlook.com>
2023-05-18 17:59:51 +0930

    Create logging.filter.ts

---
+++ src/utils/logging.filter.ts
@@ -0,0 +1,43 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpException,
+    HttpStatus,
+  } from '@nestjs/common';
+  import { Request, Response } from 'express';
+  import { captureException } from '@sentry/node';
+  
+  
+  const ignoreStatuses = [401, 403];
+  
+  @Catch()
+  export class LoggingFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+      const request = ctx.getRequest<Request>();
+      const status =
+        exception instanceof HttpException
+          ? exception.getStatus()
+          : HttpStatus.INTERNAL_SERVER_ERROR;
+      const message =
+        exception instanceof HttpException
+          ? exception.message
+          : 'Internal server error';
+  
+      let eventId: string;
+      if (!ignoreStatuses.includes(status)) {
+        eventId = captureException(exception);
+  
+        console.error(exception);
+      }
+  
+      response.status(status).json({
+        statusCode: status,
+        message,
+        eventId: eventId || undefined,
+      });
+    }
+  }
+  
